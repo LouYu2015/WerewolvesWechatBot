f47efa55b8c227268e903204396622e37f7b446e
Yu Lou <louyu27@gmail.com>
2017-03-28 20:56:28 +0800

    Added game controller class

--- werewolf_server.py
+++ werewolf_server.py
@@ -21,45 +21,196 @@ audio.audioPath = '/home/louyu/program/werewolf/audio/'
 
 test = True
 
-def main():
-    global players, identity
+class gameController:
+    def startGame(self):
+        # List of possible identities
+        if test:
+            self.identity = [Werewolf(self)]#[Villager(), Witch(), Werewolf()]
+        else:
+            self.identity = [Villager(self), Villager(self), Villager(self),\
+                Witch(self), Seer(self), Savior(self),\
+                Werewolf(self), Werewolf(self), Werewolf(self)]# Identities for each player
 
-    # List of possible identities
-    if test:
-        identity = [Werewolf()]#[Villager(), Witch(), Werewolf()]
-    else:
-        identity = [Villager(), Villager(), Villager(),\
-            Witch(), Seer(), Savior(),\
-            Werewolf(), Werewolf(), Werewolf()]# Identities for each player
+        # Shuffle identities
+        random.seed(time.time())
+        random.shuffle(self.identity)
 
-    # Shuffle identities
-    random.seed(time.time())
-    random.shuffle(identity)
+        # Initialize player list
+        self.players = [None]*(len(self.identity) + 1) # No.0 Player won't be used
 
-    # Initialize player list
-    players = [None]*(len(identity) + 1) # No.0 Player won't be used
+        # Start itchat
+        itchat.auto_login()
+        threading.Thread(target = itchat.run).start()
 
-    # Start itchat
-    itchat.auto_login()
-    threading.Thread(target = itchat.run).start()
+        # Wait for players to enter the game
+        while True:
+            print('请按回车键开始游戏')
+            input()
 
-    # Wait for players to enter the game
-    while True:
-        print('请按回车键开始游戏')
-        input()
+            can_proceed = True
+            for (i, player) in enumerate(self.players[1:]):
+                if player == None:
+                    print('缺少%d号玩家' % (i+1))
+                    can_proceed = False
 
-        can_proceed = True
-        for (i, player) in enumerate(players[1:]):
-            if player == None:
-                print('缺少%d号玩家' % (i+1))
-                can_proceed = False
+            if can_proceed:
+                break
 
-        if can_proceed:
-            break
+        # Start the game
+        playSound('游戏开始')
+        self.mainLoop()
 
-    # Start the game
-    playSound('游戏开始')
-    mainLoop()
+    def mainLoop(self):
+        nRound = 0
+        seer, savior, witch, self.werewolves = None, None, None, []
+        
+        for player in self.players[1:]:
+            if isinstance(player, Seer):
+                seer = player
+            elif isinstance(player, Witch):
+                witch = player
+            elif isinstance(player, Savior):
+                savior = player
+            elif isinstance(player, WerewolfLeader):
+                self.werewolves.insert(0, player) # Insert the leader to the front
+            elif isinstance(player, Werewolf):
+                self.werewolves.append(player)
+        
+        # Main loop
+        while True:
+            self.lastKilled = []
+            nRound += 1
+
+            self.broadcast('-----第%d天晚上-----' % (nRound-1))
+            print(log('-----第%d天晚上-----' % (nRound-1)))
+            self.broadcast('天黑请闭眼')
+            playSound('天黑请闭眼')
+
+            self.broadcast(CLR_STRING)
+            
+            self.moveFor(savior)
+                
+            for werewolf in self.werewolves:
+                if not werewolf.died:
+                    self.moveFor(werewolf)
+                    break # Only one werewolf needs to make a choice
+            
+            self.moveFor(witch)
+            
+            if witch == None:
+                self.isGameEnded()
+            
+            self.moveFor(seer)
+            
+            self.broadcast('-----第%d天-----' % nRound)
+            print(log('-----第%d天-----' % nRound))
+            self.broadcast('天亮啦')
+            playSound('天亮了')
+            
+            agent = self.players[1]
+            self.broadcast('%s 将记录白天的情况' % agent.num())
+            
+            if nRound == 1:
+                agent.inputFrom('警长竞选结束时，请按回车：')
+            
+            anyoneDied = False
+            random.shuffle(self.lastKilled)
+            for player in self.lastKilled:
+                if player.died:
+                    self.broadcast('昨天晚上，%s死了' % player.num())
+                    anyoneDied = True
+            if not anyoneDied:
+                self.broadcast('昨天晚上是平安夜～')
+
+            for player in self.lastKilled:
+                if player.died:
+                    player.afterDying()
+            
+            exploded = False
+            while True:
+                if not agent.selectFrom('是否有狼人爆炸'):
+                    self.broadcast('操作员选择无人爆炸')
+                    break
+                explodedMan = agent.selectPlayer('输入该狼人的编号')
+                if not agent.selectFrom('是否确认对方的编号是 %d' % explodedMan):
+                    continue
+                explodedMan = self.players[explodedMan]
+                if explodedMan.died:
+                    agent.message('此玩家已经死亡，不能爆炸')
+                    continue
+                if not isinstance(explodedMan, Werewolf):
+                    self.broadcast('操作员选择的不是狼！')
+                    continue
+                else:
+                    self.broadcast('操作员选择%s爆炸' % explodedMan.num())
+                    print(log('%s爆炸' % explodedMan.description()))
+                    explodedMan.die()
+                    explodedMan.afterExploded()
+                    exploded = True
+                    break
+            
+            if exploded:
+                continue
+            
+            toKill = agent.selectPlayer('选择投死的玩家编号，0为平票', minNumber = 0)
+            if toKill == 0:
+                print(log('平票，没有人被处死'))
+                self.broadcast('操作员选择了平票')
+                continue
+            toKill = self.players[toKill]
+            self.broadcast('操作员选择投死%s' % toKill.num())
+            print(log('%s被处死' % toKill.description()))
+            toKill.die()
+            toKill.afterDying()
+
+    # Check the end of game
+    def isGameEnded(self):
+        villagerCount = 0
+        godCount = 0
+        werewolfCount = 0
+
+        for player in self.players[1:]:
+            if not player.died:
+                if isinstance(player, Villager):
+                    villagerCount += 1
+                elif player.__class__.good:
+                    godCount += 1
+                else:
+                    werewolfCount += 1
+        
+        if villagerCount == 0:
+            self.broadcast('刀民成功，狼人胜利！')
+            playSound('刀民成功')
+        elif godCount == 0:
+            self.broadcast('刀神成功，狼人胜利！')
+            playSound('刀神成功')
+        elif werewolfCount == 0:
+            self.broadcast('逐狼成功，平民胜利！')
+            playSound('逐狼成功')
+        else:
+            return
+
+        print(log('游戏结束'))
+        exit()
+
+    def moveFor(self, char):
+        if char == None:
+            return
+
+        if not char.died or char in self.lastKilled:
+            char.openEyes()
+            char.move()
+            char.closeEyes()
+        else:
+            time.sleep(random.random()*4+4) # Won't let the player close eyes immediately even if he/she died.
+
+    def broadcast(self, string):
+        for player in self.players[1:]:
+            player.message(string)
+
+    def broadcastToWolves(self, string):
+        for wolf in self.werewolves:
+            wolf.message('狼人：' + string)
 
 username_to_user = {} # Map Wechat user name to WechatUser object
 
@@ -110,6 +261,7 @@ def listenText(message):
             print(log('无效的消息:%s %s\n%s' % (remarkname, username, text)))
 
 def handleRequest(user, remarkname):
+    players = controller.players
     if not remarkname:
         remarkname = user.getInput('您没有备注名，请输入你的名字')
         print('%s 更名为 %s' % (user.userName, remarkname))
@@ -132,7 +284,7 @@ def handleRequest(user, remarkname):
 
         break
         
-    players[player_id] = identity.pop() # Assign an identity
+    players[player_id] = controller.identity.pop() # Assign an identity
     
     player = players[player_id]
     player.player_id = player_id
@@ -143,171 +295,18 @@ def handleRequest(user, remarkname):
     
     print(log('%s已经上线' % players[player_id].num()))
 
-def mainLoop():
-    global players, lastKilled, nRound, werewolves
-
-    nRound = 0
-    seer, savior, witch, werewolves = None, None, None, []
-    
-    for player in players[1:]:
-        if isinstance(player, Seer):
-            seer = player
-        elif isinstance(player, Witch):
-            witch = player
-        elif isinstance(player, Savior):
-            savior = player
-        elif isinstance(player, WerewolfLeader):
-            werewolves.insert(0, player) # Insert the leader to the front
-        elif isinstance(player, Werewolf):
-            werewolves.append(player)
-    
-    # Main loop
-    while True:
-        lastKilled = []
-        nRound += 1
-
-        broadcast('-----第%d天晚上-----' % (nRound-1))
-        print(log('-----第%d天晚上-----' % (nRound-1)))
-        broadcast('天黑请闭眼')
-        playSound('天黑请闭眼')
-
-        broadcast(CLR_STRING)
-        
-        moveFor(savior)
-            
-        for werewolf in werewolves:
-            if not werewolf.died:
-                moveFor(werewolf)
-                break # Only one werewolf needs to make a choice
-        
-        moveFor(witch)
-        
-        if witch == None:
-            isGameEnded()
-        
-        moveFor(seer)
-        
-        broadcast('-----第%d天-----' % nRound)
-        print(log('-----第%d天-----' % nRound))
-        broadcast('天亮啦')
-        playSound('天亮了')
-        
-        agent = players[1]
-        broadcast('%s 将记录白天的情况' % agent.num())
-        
-        if nRound == 1:
-            agent.inputFrom('警长竞选结束时，请按回车：')
-        
-        anyoneDied = False
-        random.shuffle(lastKilled)
-        for player in lastKilled:
-            if player.died:
-                broadcast('昨天晚上，%s死了' % player.num())
-                anyoneDied = True
-        if not anyoneDied:
-            broadcast('昨天晚上是平安夜～')
-
-        for player in lastKilled:
-            if player.died:
-                player.afterDying()
-        
-        exploded = False
-        while True:
-            if not agent.selectFrom('是否有狼人爆炸'):
-                broadcast('操作员选择无人爆炸')
-                break
-            explodedMan = agent.selectPlayer('输入该狼人的编号')
-            if not agent.selectFrom('是否确认对方的编号是 %d' % explodedMan):
-                continue
-            explodedMan = players[explodedMan]
-            if explodedMan.died:
-                agent.message('此玩家已经死亡，不能爆炸')
-                continue
-            if not isinstance(explodedMan, Werewolf):
-                broadcast('操作员选择的不是狼！')
-                continue
-            else:
-                broadcast('操作员选择%s爆炸' % explodedMan.num())
-                print(log('%s爆炸' % explodedMan.description()))
-                explodedMan.die()
-                explodedMan.afterExploded()
-                exploded = True
-                break
-        
-        if exploded:
-            continue
-        
-        toKill = agent.selectPlayer('选择投死的玩家编号，0为平票', minNumber = 0)
-        if toKill == 0:
-            print(log('平票，没有人被处死'))
-            broadcast('操作员选择了平票')
-            continue
-        toKill = players[toKill]
-        broadcast('操作员选择投死%s' % toKill.num())
-        print(log('%s被处死' % toKill.description()))
-        toKill.die()
-        toKill.afterDying()
-
-def moveFor(char):
-    if char == None:
-        return
-    if not char.died or char in lastKilled:
-        char.openEyes()
-        char.move()
-        char.closeEyes()
-    else:
-        time.sleep(random.random()*4+4) # Won't let the player close eyes immediately even if he/she died.
-
 # Add time to message
 def log(string):
     return '[%s]%s' % (time.strftime('%H:%M:%S'), string)
 
-# Broadcast
-def broadcast(string):
-    for player in players[1:]:
-        player.message(string)
-
-def broadcastToWolves(string):
-    for wolf in werewolves:
-        wolf.message('狼人：' + string)
-
-# Check the end of game
-def isGameEnded():
-    villagerCount = 0
-    godCount = 0
-    werewolfCount = 0
-
-    for player in players[1:]:
-        if not player.died:
-            if isinstance(player, Villager):
-                villagerCount += 1
-            elif player.__class__.good:
-                godCount += 1
-            else:
-                werewolfCount += 1
-    
-    if villagerCount == 0:
-        broadcast('刀民成功，狼人胜利！')
-        playSound('刀民成功')
-    elif godCount == 0:
-        broadcast('刀神成功，狼人胜利！')
-        playSound('刀神成功')
-    elif werewolfCount == 0:
-        broadcast('逐狼成功，平民胜利！')
-        playSound('逐狼成功')
-    else:
-        return
-
-    print(log('游戏结束'))
-    exit()
-
 class Character:
-    def __init__(self):
+    def __init__(self, controller):
         self.player_id = None # ID of the player
         self.died = False # Is player died
         self.protected = False # Is player protected by Savior
         self.name = None # Player's name
         self.user = None # WechatUser object
+        self.controller = controller
     
     def welcome(self):# Tell the player his/her identity
         self.message('你是%s' % self.description())
@@ -335,13 +334,13 @@ class Character:
                 self.message('这不是数字')
                 continue
                 
-            if not(answer >= minNumber and answer < len(players)):
+            if not(answer >= minNumber and answer < len(self.controller.players)):
                 self.message('超出编号范围')
                 continue
             return answer
     
     def kill(self):# Try to kill the player
-        lastKilled.append(self)
+        self.controller.lastKilled.append(self)
         if not self.protected:
             self.die(True)
         return self.died
@@ -349,8 +348,8 @@ class Character:
     def die(self, killedByWolf = False):# Make the player died
         self.died = True
         if not killedByWolf:
-            lastKilled.append(self)
-            isGameEnded()
+            self.controller.lastKilled.append(self)
+            self.controller.isGameEnded()
     
     def afterDying(self):
         pass
@@ -372,8 +371,8 @@ class Witch(Character):
     identity = '女巫'
     good = True
     
-    def __init__(self):
-        super().__init__()
+    def __init__(self, controller):
+        super().__init__(controller)
         self.usedPoison = False
         self.usedMedicine = False
     
@@ -382,7 +381,7 @@ class Witch(Character):
             self.message('你用过解药了')
         else:
             try:
-                diedMan = lastKilled[-1]
+                diedMan = self.controller.lastKilled[-1]
             except IndexError:
                 self.message('刚才是空刀')
             else:
@@ -400,7 +399,7 @@ class Witch(Character):
                     if diedMan.protected:
                         print(log('同守同救！'))
                         diedMan.died = True
-        isGameEnded()
+        self.controller.isGameEnded()
             
         if self.usedPoison:
             time.sleep(random.random()*4+4)# Won't let the player close eyes immediately
@@ -409,7 +408,7 @@ class Witch(Character):
             if self.selectFrom('是否使用毒药'):
                 manToKill = self.selectPlayer('输入要毒死的玩家,0表示取消', minNumber = 0)
                 if manToKill != 0:
-                    manToKill = players[manToKill]
+                    manToKill = self.controller.players[manToKill]
                     print(log('%s毒死了%s' % (self.description(), manToKill.description())))
                     manToKill.die()
                     manToKill.canUseGun = False
@@ -419,14 +418,14 @@ class Savior(Character):
     identity = '守卫'
     good = True
     
-    def __init__(self):
-        super().__init__()
-        self.lastProtected = players[0]
+    def __init__(self, controller):
+        super().__init__(controller)
+        self.lastProtected = self.controller.players[0]
     
     def move(self):
         while True:
             protectedMan = self.selectPlayer('输入要守护的人,0表示空守', minNumber = 0)
-            protectedMan = players[protectedMan]
+            protectedMan = self.controller.players[protectedMan]
             
             if protectedMan.player_id != 0 and protectedMan is self.lastProtected:
                 self.message('连续的回合里不能守护同一个人')
@@ -445,8 +444,8 @@ class Seer(Character):
     def move(self):
         while True:
             watchedMan = self.selectPlayer('选择你要预言的人')
-            watchedMan = players[watchedMan]
-            if watchedMan.died and watchedMan not in lastKilled:
+            watchedMan = self.controller.players[watchedMan]
+            if watchedMan.died and watchedMan not in self.controller.lastKilled:
                 self.message('你不能验死人')
                 continue
             break
@@ -458,10 +457,11 @@ class Seer(Character):
             print(log('%s发现%s是查杀' % (self.description(), watchedMan.description())))
             
 class Hunter(Character):
-    self.identity = '猎人'
-    self.good = True
-    def __init__(self):
-        super().__init__()
+    identity = '猎人'
+    good = True
+
+    def __init__(self, controller):
+        super().__init__(controller)
         self.canUseGun = True
     
     def afterDying(self):
@@ -475,7 +475,7 @@ class Hunter(Character):
             print(log('猎人没有放枪'))
             return
         
-        player = players[player]
+        player = self.controller.players[player]
         
         broadcast('%s枪杀了%s' % (self.description(), player.num()))
         print(log('%s枪杀了%s' % (self.description(), player.description())))
@@ -486,15 +486,15 @@ class Werewolf(Character):
     good = False
     
     def move(self):
-        broadcastToWolves('由%s代表狼人进行操作' % self.description())
+        self.controller.broadcastToWolves('由%s代表狼人进行操作' % self.description())
         killedMan = self.selectPlayer('输入你要杀的玩家，0表示空刀', minNumber = 0)
         if killedMan == 0:
-            broadcastToWolves('狼人选择空刀')
+            self.controller.broadcastToWolves('狼人选择空刀')
             print(log('狼人空刀'))
             return
-        killedMan = players[killedMan]
+        killedMan = self.controller.players[killedMan]
         killedMan.kill()
-        broadcastToWolves('狼人选择刀%s' % killedMan.num())
+        self.controller.broadcastToWolves('狼人选择刀%s' % killedMan.num())
         print(log('狼人刀%s' % killedMan.description()))
     
     def afterExploded(self):
@@ -506,7 +506,7 @@ class WerewolfLeader(Werewolf):
     def afterExploded(self):
         if self.selectFrom('是否要带人'):
             killedMan = self.selectPlayer('输入你要带走的人')
-            killedMan = players[killedMan]
+            killedMan = self.controller.players[killedMan]
             killedMan.die()
             broadcast('%s带走了%s' % (self.description(), killedMan.num()))
             print(log('%s带走了%s' % (self.description(), killedMan.description())))
@@ -521,4 +521,5 @@ class Villager(Character):
     identity = '村民'
     good = True
 
-main()
+controller = gameController()
+controller.startGame()
